3b88e7d21b83da0d9bcdb60af7c7c2c4ddcadadf
Arshdeep Singh <arsh@arshdeeps-air.mynetworksettings.com>
2025-09-26 20:39:39 -0400

    trying

--- src/transactions/DeleteSong_Transaction.js
+++ src/transactions/DeleteSong_Transaction.js
@@ -1,19 +1,21 @@
+// src/transactions/DeleteSong_Transaction.js
 export default class DeleteSong_Transaction {
   constructor(app, index) {
     this.app = app;
-    this.song = song;
     this.index = index;
+    this.removedSong = null; // capture the deleted song during do
   }
+
   doTransaction() {
     const songs = [...this.app.state.currentList.songs];
-    const pos = this.index ?? songs.length;
-    songs.splice(pos, 0, this.song);
+    this.removedSong = songs[this.index];
+    songs.splice(this.index, 1);
     this.app.updateCurrentListSongs(songs);
-    this.index = pos;
   }
+
   undoTransaction() {
     const songs = [...this.app.state.currentList.songs];
-    songs.splice(this.index, 1);
+    songs.splice(this.index, 0, this.removedSong);
     this.app.updateCurrentListSongs(songs);
   }
 }
